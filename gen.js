d5b6bf71fd0c0a0b166bdee66adcd65fcf40de54
alzanati <hosny1993.bio@gmail.com>
2018-04-28 01:44:22 +0200

    add gen.js to generate new any algorithm folder

---
+++ gen.js
@@ -0,0 +1,32 @@
+let fs = require('fs');
+let readline = require('readline');
+
+const fileheader = '/**\n * @author:\n * @description:\n */';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What you want to implement: ', (algorithm_name) => {
+    fs.mkdir(algorithm_name, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(algorithm_name + ' directory created...');
+            createTestFile(algorithm_name, 'index');
+            createTestFile(algorithm_name, 'test');
+        }
+    });
+    rl.close();
+});
+
+function createTestFile(algorithm_name, file_name) {
+    fs.writeFile(algorithm_name + '/' + file_name + '.js', fileheader, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(file_name + '.js file created...');
+        }
+    });
+}

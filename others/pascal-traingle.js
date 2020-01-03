65b751ef029b2e2c97bae362a73ee86d8a0c1cd2
hosny1993 <hosny1993.bio@gmail.com>
2020-01-03 18:08:01 +0200

    add pascal traingle algorithm

---
+++ others/pascal-traingle.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    if (!numRows || numRows === 0) return [];
+    if (numRows === 1) return [
+        [1]
+    ];
+    if (numRows === 2) return [
+        [1],
+        [1, 1]
+    ];
+
+    let result = [[1], [1, 1]];
+    for (let n = 3; n <= numRows; n++) {
+
+        let current = [];
+        let last = initArr(result[result.length - 1]);
+
+        for (let j = 0; j < last.length; j++) {
+            if (j + 1 >= last.length) {
+                break;
+            }
+            current.push(last[j] + last[j + 1]);
+        }
+
+        result.push(current);
+    }
+
+    return result;
+};
+
+var initArr = function (arr) {
+    let result = [0];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i]);
+    }
+
+    result.push(0);
+    return result;
+};
+
+console.log(generate(5));
